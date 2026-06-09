79db70c8b0f3d108ec710a01488a81d0c0d97bfc
Naruf <102542599+Naruf@users.noreply.github.com>
2026-06-09 20:32:57 +0200

    User experience grooming
    - Search section has been divided into 2 sections: search-container and result-container
    - Result container pops up when the submit button is clicked
    - Blinking message while waiting for the AI engine result
    - Some other details were groomed in order to improve the UX

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ function showCityInfo(response) {
   new Typewriter("#result", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 25,
+    delay: 20,
     cursor: null,
   });
 }
 function getCityInfo(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#search-input");
-  let city = cityElement.value;
+  let cityTrimmed = cityElement.value.trim();
+  let city = cityTrimmed
+    .toLowerCase()
+    .split(" ")
+    .filter((word) => word !== "")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
   let apiKey = "5d1t76143df0603191aa4604b0b5b1oe";
-  let prompt = `Display a paragragh with the structure: <h3/><These are some place in ${city} that offer gluten-free options:<h3/> followed by <ol><li><li/><ol/>, <li><li/> should show a result for:1. a restaurant , 2.a bakery , 3.a cafe , 4.a supermarket. Use data from Google and filter by keyword:"gluten-free" in ${city}. For each result, display: <name of the restaurant><br/> street and number<br/> the place website or social media Url, adding <target= "_blank"> attribute to <a><a/> element. Justify <h3><h3/> and <ol><ol/> to the left. Do not show AI intro. Discard results from bussiness with no Google reviews in the last year.`;
-  let context = `You are an expert in gluten-free products advising someone that is new to the city and it is looking for restautants, cafes, bakeries and supermarkets where they are offered gluten-free products.  Use real bussiness data.Do not make up the data.`;
+  let prompt = `Display a paragragh with the structure: <h4/><These are some place in ${city} that offer gluten-free options:<h4/> followed by <ol><li><li/><ol/>, <li><li/> should show a result for:1. a restaurant , 2.a bakery , 3.a cafe , 4.a supermarket. Use data from Google and filter by keyword:"gluten-free" in ${city}. For each result, display: <name of the restaurant><br/> street and number<br/> the place website or social media Url, adding <target= "_blank"> attribute to <a><a/> element. Justify <h4><h4/> and <ol><ol/> to the left. Set the css property margin-top to 5px, margin-bottom to 5px. Do not show AI intro. Print at the bottom in a new line:"SheCodes AI" in color rgb(203, 34, 119) and set the padding-left to 30px, padding-top to 20px.`;
+  let context = `You are an expert in gluten-free products advising someone that is new to the city and it is looking for restautants, cafes, bakeries and supermarkets where they are offered gluten-free products. Use real bussiness data. Do not make up the data.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showCityInfo);
+  let resultSection = document.querySelector("#result");
+  resultSection.classList.remove("hidden");
+  resultSection.innerHTML = `<div class="waitingForResult"><strong>The information about ${city} is being generated ⌛<strong/><div/>`;
 }
 
 let searchInfoElement = document.querySelector("#city-name");
